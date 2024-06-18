41d08850734323cb9465b86f4eca11c8af046887
Nguyen Huyen <huyennguyen.uet@vnu.edu.vn>
2024-06-18 22:25:05 +0900

    Add join page

---
+++ WebApp/Pages/Othello/WaitingJoin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.Othello
+{
+    public class WaitingJoinModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
